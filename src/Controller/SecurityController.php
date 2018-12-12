fb7bee1dbcb97be9aa6a46929f32f351ff838fb6
JanMaria <jan.maria.prokop@gmail.com>
2018-12-12 18:49:19 +0100

    wysyłanie wiadomości email przy nieudanej próbie logowania

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,18 +6,33 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use App\Entity\User;
+use \Swift_Message;
 
 class SecurityController extends AbstractController
 {
     /**
      * @Route("/login", name="app_login")
      */
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils, \Swift_Mailer $mailer): Response
     {
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
+
+        if ($error !== null &&
+          $this->getDoctrine()->getManager()->getRepository(User::class)->findOneBy(['email' => $lastUsername]) !== null) {
+            $message = (new Swift_Message())
+              ->setSubject('[ytcrud]: Nieudana próba logowania')
+              ->setFrom('ytcrud')
+              ->setTo($lastUsername)
+              ->setBody('Ktoś próbował się zalogować na twoje konto przy użyciu niepoprawnego hasła.');
+
+              $mailer->send($message);
+
+              $this->addFlash('loginFailure', 'Niepoprawne hasło. Wysłano powiadomienie.');
+        }
 
         return $this->render('security/login.html.twig', [
           'last_username' => $lastUsername,
